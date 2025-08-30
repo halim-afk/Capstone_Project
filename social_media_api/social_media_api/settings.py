2ea1e720f5bd6a3d7e7680b9856309b2ea5ba4f8
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-30 14:31:02 +0100

    fix some problem security

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_%ewur(+7-yj)roe&13wj5xleul^^luxe=q#wnh*03j+@*&ou6'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/',]
 
 
 # Application definition
