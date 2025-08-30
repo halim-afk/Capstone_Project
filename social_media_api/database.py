48b8268bd3e61152ea0b477de0f927893d08d933
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-30 14:40:08 +0100

    fix some problem security

--- social_media_api/database.py
+++ social_media_api/database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
+from decouple import config
 
 def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
@@ -15,10 +16,13 @@ def create_db_connection(host_name, user_name, user_password, db_name):
         print(f"Error: '{err}'")
 
     return connection
-
+db_host = config('DB_HOST')
+db_user = config('DB_USER')
+db_password = config('DB_PASSWORD')
+db_name = config('DB_NAME')
 # استخدام الدالة للاتصال
 # تأكد من استبدال القيم أدناه بمعلومات قاعدة بياناتك
-connection = create_db_connection("localhost", "root", "your_password", "social_media")
+connection = create_db_connection(db_host, db_user, db_password, db_name)
 
 # يمكنك الآن استخدام الكائن connection لإرسال استعلامات SQL
 # مثال:
